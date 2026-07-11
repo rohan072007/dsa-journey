8a1f03c031010926318c15fd34841d15c3c314d9
rohan072007 <playsbrust@gmail.com>
2026-07-11 23:16:28 +0530

    more question

--- pattern and logic/pattern2.cpp
+++ pattern and logic/pattern2.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 void print(int n)
 {
-
     for (int i = 0; i < n; i++)
     {
+        
         for (int j = 0; j <= i; j++)
         {
             cout << " * ";
@@ -99,13 +99,116 @@ void print7(int n)
         cout << endl;
     }
 }
-
-
-int main()
+void print8(int n)
 {
-    int n = 10;
- print6(n);
-    print7(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<=n;i++){
+        for(int j=i;j<n-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    } 
+}
+void print9(int n){
+    int start=1;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            start =1;
+        }else{
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<<start;
+            start = 1-start;
+        }
+        cout<<endl;
+    }
+}
+void print10(int n){
+    for(int i=1;i<n;i++){
+        int space= 2*n-2*i;
+        //numbers
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        
+        //spaces
+        for(int j=1;j<=space;j++){
+            cout<<"_";
+        }
+        //numbers
+        for(int j=i;j>=1;j--){
+            cout<<i;
+        }
+        cout<<endl;
+    
+
+}
+}
+void print11(int n){
+    int count = 1;
+    for(int i=1;i<=n;i++){
+       for(int j=1;j<=i;j++){
+           cout<<count;
+           count++;
+       }
+       cout<<endl;
+}
+}
+void print12(int n){
+    
+    for(int i=0;i<=n;i++){
+        for(char ch='A';ch<='A'+(n-i-1);ch++){
+            cout<<ch<<" ";
+           
+        }
+        cout<<endl;
+    }
+}
+void print13(int n){
+    
+    for(int i=0;i<=n;i++){
+        for(char ch='A';ch<='A'+i;ch++){
+            cout<<ch<<" ";
+           
+        }
+        cout<<endl;
+    }
+}
+void print14(int n){
+    for(int i=0;i<n;i++){
+        char ch='A'+i;
+        for(int j=0;j<=i;j++){
+            cout<<ch<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+
+void print15(int n){
+    int a=65;
+    for(int i=0;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            cout<<char(a)<<" ";
+           
+        }
+        a++;
+        cout<<endl;
+    }
+}int main()
+{
+    int n = 5;
+    print14(n);
+    
 
     return 0;
 }
